95578ee679da38777753147bf1e15a6efde6f193
gamemanager2 <mishamkr0@gmail.com>
2019-06-29 22:53:19 +0300

    Add project files.

---
+++ mm6/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace mm6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(212, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сеточка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "side of square";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 54);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 115);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "А ну, работать!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 115);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Стоямба";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 210);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Выйти и получить 50";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 518);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ mm6/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mm6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public int[,] matrix;
+        public int[,] matrix2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.Clear(Color.White);
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            int widthLines = Convert.ToInt32(textBox1.Text);
+            int heightLines = widthLines;
+            matrix = new int[500 / heightLines, 750 / widthLines];
+            matrix2 = new int[500 / heightLines, 750 / widthLines];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 0;
+                    matrix2[i, j] = 0;
+                }
+            for (int i = 0; i < w; i += widthLines)
+            {
+                gr.DrawLine(new Pen(Brushes.Black), new Point(i + widthLines, 0), new Point(i + widthLines, h));
+                gr.DrawLine(new Pen(Brushes.Black), new Point(0, i + heightLines), new Point(w, i + heightLines));
+            }
+           // textBox1.Text = matrix.GetLength(0).ToString();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics gr = pictureBox1.CreateGraphics();
+            matrix[Convert.ToInt32(e.Y / Convert.ToInt32(textBox1.Text)), Convert.ToInt32(e.X / Convert.ToInt32(textBox1.Text))] = 1;
+            gr.FillRectangle(Brushes.Blue, Convert.ToInt32(e.X / (pictureBox1.Width / matrix.GetLength(1))) * Convert.ToInt32(textBox1.Text), Convert.ToInt32(e.Y / (pictureBox1.Height / matrix.GetLength(0))) * Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix2[i, j] = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int individual = 0;
+                    if (i - 1 < 0)
+                    {
+                        if (j - 1 < 0)
+                        {
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i + 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                        }
+                        else
+                        if (j + 1 > matrix.GetLength(1) - 1)
+                        {
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i, 0] == 1)
+                                individual++;
+                            if (matrix[i + 1, 0] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, 0] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j - 1] == 1)
+                                individual++;
+                        }
+                        else
+                        {
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i + 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[matrix.GetLength(0) - 1, j + 1] == 1)
+                                individual++;
+                        }
+                    }
+                    else if (i + 1 > matrix.GetLength(0) - 1)
+                    {
+                        if (j - 1 < 0)
+                        {
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i - 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[0, j] == 1)
+                                individual++;
+                            if (matrix[0, j + 1] == 1)
+                                individual++;
+                            if (matrix[0, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                        }
+                        else
+                        if (j + 1 > matrix.GetLength(1) - 1)
+                        {
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i, 0] == 1)
+                                individual++;
+                            if (matrix[i - 1, 0] == 1)
+                                individual++;
+                            if (matrix[0, 0] == 1)
+                                individual++;
+                            if (matrix[0, j] == 1)
+                                individual++;
+                            if (matrix[0, j - 1] == 1)
+                                individual++;
+                        }
+                        else
+                        {
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i - 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[0, j] == 1)
+                                individual++;
+                            if (matrix[0, j - 1] == 1)
+                                individual++;
+                            if (matrix[0, j + 1] == 1)
+                                individual++;
+                        }
+                    }
+                    else
+                    {
+                        if (j - 1 < 0)
+                        {
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i - 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, matrix.GetLength(1) - 1] == 1)
+                                individual++;
+                        }
+                        else
+                        if (j + 1 > matrix.GetLength(1) - 1)
+                        {
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i - 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i, 0] == 1)
+                                individual++;
+                            if (matrix[i + 1, 0] == 1)
+                                individual++;
+                            if (matrix[i - 1, 0] == 1)
+                                individual++;
+                        }
+                        else
+                        {
+                            if (matrix[i, j - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j - 1] == 1)
+                                individual++;
+                            if (matrix[i - 1, j] == 1)
+                                individual++;
+                            if (matrix[i - 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i, j + 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j + 1] == 1)
+                                individual++;
+                            if (matrix[i + 1, j] == 1)
+                                individual++;
+                            if (matrix[i + 1, j - 1] == 1)
+                                individual++;
+                        }
+
+                    }
+                    if (matrix[i, j] == 0)
+                    {
+                        if (individual == 3)
+                        {
+                            matrix2[i, j] = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (individual == 2 || individual == 3)
+                            matrix2[i, j] = 1;
+                    }
+                }
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            gr.Clear(Color.White);
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+            int widthLines = Convert.ToInt32(textBox1.Text);
+            int heightLines = widthLines;
+            for (int i = 0; i < w; i += widthLines)
+            {
+                gr.DrawLine(new Pen(Brushes.Black), new Point(i + widthLines, 0), new Point(i + widthLines, h));
+                gr.DrawLine(new Pen(Brushes.Black), new Point(0, i + heightLines), new Point(w, i + heightLines));
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = matrix2[i, j];
+                    if (matrix2[i, j] == 1)
+                    {
+                        gr.FillRectangle(Brushes.Blue, j * Convert.ToInt32(textBox1.Text), i * Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text));
+                    }
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
